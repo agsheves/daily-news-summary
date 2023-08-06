a8ab157676184a0e752486ac8593c1e8a6d1a837
andrew@tarjumansolutions.com <andrew@tarjumansolutions.com>
2023-08-06 13:58:05 +0000

    Created botpress and botPress.html; Edited theme.css, theme/parameters.yaml, Home.Form1 and Home

--- client_code/botpress/__init__.py
+++ client_code/botpress/__init__.py
@@ -1,15 +1,14 @@
-from ._anvil_designer import HomeTemplate
+from ._anvil_designer import botpressTemplate
 from anvil import *
+import anvil.server
 import anvil.users
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.server
-import anvil.js
 
-class Home(HomeTemplate):
+class botpress(botpressTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
