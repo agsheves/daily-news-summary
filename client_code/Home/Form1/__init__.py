e6af8d6ef7e11143cb46759504efe4ec2a19b4e5
andrew@tarjumansolutions.com <andrew@tarjumansolutions.com>
2023-08-06 13:13:55 +0000

    Edited theme.css and Home.Form1

--- client_code/Home/Form1/__init__.py
+++ client_code/Home/Form1/__init__.py
@@ -12,8 +12,11 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.column_panel_1.role = 'floating-panel'
 
     # Any code you write here will run before the form opens.
+  
+
 
   def get_stories_click(self, **event_args):
     """This method is called when the button is clicked"""
